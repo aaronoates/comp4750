5466d546504ee348d2aabc2237fbc49f960931eb
aaronoates <aaronoates2003@gmail.com>
2024-09-16 10:45:51 -0230

    assignment1 question 2 completed
    code works as desired.

--- tcomp2.py
+++ tcomp2.py
@@ -6,55 +6,55 @@
 ##              MUN #: 202105417                       ##
 #########################################################
 import sys
-from collections import defaultdict
 
 def read_words(filepath):
     """
     Reads the contents of the file and splits them into words based on whitespace.
     """
     with open(filepath, 'r') as f:  # Opens and reads from the designated filepath
-        return f.read().split()  # Reads the contents of the file and splits by whitespace
+        return set(f.read().split())  # Reads the contents of the file and splits by whitespace
     
 def num_Of_Words(words):
-    """
-    gets the number of different words in a text
-    """
-    different_words = []
-    for word in words:
-        if word not in different_words:
-            different_words.append(word)# if a new word is discovered, a new key for this word is created in the dictionary and the count is initialized as 1.
-       
+    
 
-    num_of_words = len(different_words)
+    num_of_words = len(words)
 
     return num_of_words
 
 def SD(X,Y):
-    x_list = []
-    y_list = []
-    for word in X:
-        if word not in X:
-            x_list.append(word)
     
-    for word in Y:
-        if word not in Y:
-            y_list.append(word)
-        
+    all_words = X.symmetric_difference(Y) #creates a set of all the words that are in X or Y but not both.   
 
-    all_words = set(x_list).union(set(y_list))
+    
 
-    sd = len(all_words)
+    sd = len(all_words) #Gets the number of words in the above set.
     return sd
 
 def sim(X,Y):
-    Sim = 1.0 - (SD(X,Y)/(num_Of_Words(X) + num_Of_Words(Y)))
-    print(f'Sim("{X}","{Y}") = {Sim}')
+    Sim = 1.0 - (SD(X,Y)/(num_Of_Words(X) + num_Of_Words(Y))) #based on the formula in the assignment text.
+    return Sim
 
 
 
 def main():
-    if len(sys.argv) < 3:  # Ensure there are at least 4 command-line arguments
-        print("usage: python3 tcomp3.py filename file1name file2name...")
+    if len(sys.argv) < 3:  # Ensure there are at least 3 command-line arguments
+        print("usage: python3 tcomp2.py filename file1name file2name...")
         return
     
+    most_similar_file = None #To be assigned later
+    best_sim_score = 0.0 #Default value assignment
+    master_file = sys.argv[1] #The master file is the second command line argument
+    comparison_files = sys.argv[2:] #All arguments from the third one onwards are the comparison files.
+    master_words = read_words(master_file) 
+    for comparison_file in comparison_files:
+        comparison_words = read_words(comparison_file) 
+        sim_score = sim(master_words, comparison_words)
+        print(f'>>> Sim("{master_file}", "{comparison_file}") = {sim_score:.3f}') #Prints the Sim score of the master file and the current comparison file to 3 decimal places
+        if sim_score > best_sim_score:
+            best_sim_score = sim_score #this sim score becomes the new current best.
+            most_similar_file = comparison_file # this comparison file becomes the new most similar.
+    print(f'File "{most_similar_file}" is most similar to "{master_file}"')
     
+
+if __name__ == "__main__":
+    main()
